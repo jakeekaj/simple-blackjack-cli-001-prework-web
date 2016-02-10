d86fa8c245f66f7e397d990479d0a2259944d1f3
jakeekaj <jakemalto2014@gmail.com>
2016-02-10 15:56:31 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,52 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  x = deal_card
+  x = x + deal_card
+  display_card_total(x)
+  x
 end
 
-def hit?
-  # code hit? here
+def hit?(current_total)
+  prompt_user
+  input = get_user_input
+  if input == "h"
+    x = deal_card
+    display_card_total(x + current_total)
+    x + current_total
+    elsif input == "s"
+
+      current_total
+    else
+      invalid_command
+      prompt_user
+  end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +54,12 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  x = initial_round
+
+until x > 21 do
+  x = hit?(x)
+  end
+end_game(x)
 end
-    
+
